f5c6dbd325bc542e96f5415f9c07ed3b710a7337
LUCIFER <99268911+luci53@users.noreply.github.com>
2023-08-27 17:57:51 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,55 @@
+fx_version 'cerulean'
+games      { 'gta5' }
+lua54 'yes'
+
+author 'Luicer'
+description 'Outfit bag script by Lucifer'
+version '1.0'
+
+
+ui_page 'html/index.html'
+
+--
+-- Files
+--
+
+files {
+    'html/js/jquery.js',
+    'html/js/jquery-ui.js',
+    'html/fonts/quicksand.ttf',
+    'html/img/*.png',
+    'html/index.html',
+}
+
+--- UNCOMMENT IF YOU'RE USING OLD QB CORE EXPORT
+-- shared_script '@qb-core/import.lua'
+
+--
+-- Server
+--
+
+server_scripts {
+    --- !!! COMMENT OUT IF YOU'RE USING QBCORE !!!
+    '@mysql-async/lib/MySQL.lua',
+    -----------------------------------------------
+
+    'locale/locale.lua',
+    'config.lua',
+    'server/editable/esx.lua',
+    'server/editable/qb.lua',
+    'server/server.lua',
+}
+
+--
+-- Client
+--
+
+client_scripts {
+    'locale/locale.lua',
+    'config.lua',
+    'client/editable/settings.lua',
+    'client/functions.lua',
+    'client/editable/editable.lua',
+    'client/client.lua',
+    'client/editable/target.lua',
+}
